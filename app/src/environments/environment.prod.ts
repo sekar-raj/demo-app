81249a2c28a2bad0d73e57bc116f7c0a7755a031
ganasekaran <ganasekaran09@gmail.com>
2020-11-02 16:56:55 +0530

    initail commit

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    "name": "prod",
+    "properties": {
+        "production": true,
+        "ssdURL": "http://localhost:8081/api/",
+        "tenantName": "expleo",
+        "appName": "demo-app",
+        "namespace": "com.expleo.demo-app",
+        "googleMapKey": "AIzaSyCSTnVwijjv0CFRA4MEeS-H6PAQc87LEoU",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web"
+    }
+}
---
+++ server/src/config/config.ts
@@ -0,0 +1,8 @@
+export default {
+  logger: { level: 'debug', transport: ['file', 'console'] },
+  middlewares: {
+    pre: [{ __ssdGlobalMiddlewares__: 'sd_Tp9jNbUvUZ1kJnAn' }],
+    post: [],
+    sequences: {}
+  }
+};
